422fb9822915eb8ccd56c299ec09e532d6a4c53e
jonathanx111 <jonathanx111@gmail.com>
2020-12-09 20:53:18 -0500

    fina

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(nameArray, event) {
+    let arr = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        arr.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+// function countDown(num) {
+//     for (let i = 0; i <= num; i++ ) {
+//         console.log(num - i)
+//     }
+// }
+
+function countDown(num) {
+    let i = 0;
+    while (i <= 10) {
+        console.log(num - i);
+        i++;
+    }
+}
